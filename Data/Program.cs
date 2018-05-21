48dec724150c4e97aff8648761f47a20a03ceddd
JensMalm <jensmalm873@gmail.com>
2018-05-21 13:20:50 +0200

    Database osv

---
+++ Data/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CrimeContext : DbContext
+    {
+        public DbSet<User> Customer { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\mssqllocaldb; Database = EfCrime; Trusted_Connection = True; ");
+        }
+
+
+    }
+}
