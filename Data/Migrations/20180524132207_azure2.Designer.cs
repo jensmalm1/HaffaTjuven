cae3bf11fab93bed783158d3aa77022ebc967aca
JensMalm <jensmalm873@gmail.com>
2018-05-24 15:25:33 +0200

    azure funkar 2

--- Data/Migrations/20180524132207_azure2.Designer.cs
+++ Data/Migrations/20180524132207_azure2.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Data.Migrations
 {
     [DbContext(typeof(CrimeContext))]
-    partial class CrimeContextModelSnapshot : ModelSnapshot
+    [Migration("20180524132207_azure2")]
+    partial class azure2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20180524132207_azure2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Data.Migrations
+{
+    public partial class azure2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CrimeId",
+                table: "Informations",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CrimeId",
+                table: "Informations");
+        }
+    }
+}
