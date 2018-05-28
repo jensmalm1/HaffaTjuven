5f0f827a089ad6aaead2dfe29c23880fc3e51da5
JensMalm <jensmalm873@gmail.com>
2018-05-28 13:37:47 +0200

    Login

---
+++ API/wwwroot/js/login.js
@@ -0,0 +1,49 @@
+﻿<? php
+
+$user = 'jonny4';
+$pass = 'delafoo';
+
+
+$("#showUsersButton").click(function () {
+
+    $.ajax({
+        url: "/api/ShowUsers",
+
+        method: 'get',
+
+        success: function (result) {
+
+            $user.;
+        },
+
+        error: function (xhr, status, error) {
+
+            alert("Nu blev det fel");
+        }
+    });
+});
+
+if (isset($_POST['username']) && isset($_POST['password')) {
+
+    if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) {
+
+        if (isset($_POST['rememberme'])) {
+            /* Set cookie to last 1 year */
+            setcookie('username', $_POST['username'], time() + 60 * 60 * 24 * 365, '/account', 'www.example.com');
+            setcookie('password', md5($_POST['password']), time() + 60 * 60 * 24 * 365, '/account', 'www.example.com');
+
+        } else {
+            /* Cookie expires when browser closes */
+            setcookie('username', $_POST['username'], false, '/account', 'www.example.com');
+            setcookie('password', md5($_POST['password']), false, '/account', 'www.example.com');
+        }
+        header('Location: index.php');
+
+    } else {
+        echo 'Username/Password Invalid';
+    }
+
+} else {
+    echo 'You must supply a username and password.';
+}
+?>
