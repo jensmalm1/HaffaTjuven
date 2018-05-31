581e908cbb17dde19a8432a7a3056488475bec5d
ACADEMY-7464B36\Administrator <annapiatoresson@hotmail.com>
2018-05-31 10:44:20 +0200

    testar tester

--- ValidateUserName.Test/UnitTest1.cs
+++ ValidateUserName.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Runtime.InteropServices.ComTypes;
+using API.Controllers;
 using Domain;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ValidateUserName.Test
@@ -12,20 +14,44 @@ namespace ValidateUserName.Test
         [TestMethod]
         public void TestingUserNameAndPassword_()
         {
-            var user = new User("Arvid", "123");
+            var user = new User {UserName = "Arvid", Password = "123"};
             var expectedName = "Arvid";
             var expectedPassword = "123";
             Assert.AreEqual(user.UserName, expectedName);
             Assert.AreEqual(user.Password,expectedPassword);
 
 
-        //}
+        }
         //[TestMethod]
         //[ExpectedException(typeof(Exception))]
         //public void TextingUserNameAndPassword()
         //{
-        //    var user = new User("", "123");
-        //    Assert.AreEqual(user.UserName, "");
+        //    var user = new User{UserName = "Arvid"};
+        //var user = new User { UserName = "Laban", Password = null };
+        //var metod = new CrimeController().AddUser(user);
+        //Assert.ThrowsException<FormatException>(()=>metod);
+        //    Assert.AreEqual(user,"Per");
+
+
+
+
+        //}
+        [TestMethod]
+        public void TestingAddingNewUserWithoutPassword_expectedException()
+        {
+            var user = new User { UserName = "Laban", Password = null };
+            var metod = new CrimeController().AddUser(user);
+            Assert.ThrowsException<FormatException>(()=>metod);
+
+
+
+        }
+        //[TestMethod]
+        //public void Test()
+        //{
+        //    var user = new User { UserName = "Laban" };
+        //    var metod = new CrimeController().AddUser(user);
+        //    Assert.ThrowsException<BadRequestResult>(() => metod);
 
 
 
