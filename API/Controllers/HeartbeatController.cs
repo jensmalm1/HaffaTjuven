453373d21237af4b786b8a0570e71830e5aca968
Amanda Westerlund <Westerlund922@gmail.com>
2018-05-31 11:59:32 +0200

    Gjort Heartbeatcontroller som kollar om databasen är uppe.

---
+++ API/Controllers/HeartbeatController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("heartbeat")]
+    public class HeartbeatController : Controller
+    {
+        private readonly CrimeContext _context;
+
+        public HeartbeatController(CrimeContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IActionResult SiteIsRuning()
+        {
+            return Ok("Site is up");
+        }
+
+        [HttpGet("database")]
+        public IActionResult DatabaseIsRunning()
+        {
+            
+                try
+                {
+                    _context.Database.OpenConnection();
+                    _context.Database.CloseConnection();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+
+                return Ok("Database is running");
+            
+            
+        }
+    }
+}
